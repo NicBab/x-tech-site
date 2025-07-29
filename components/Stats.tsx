c618259952866ed4749059d257bce8d62808e4e0
nickpbab@gmail.com <Nick_Bab>
2025-07-29 16:12:48 -0500

    commented out STATS components

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -1,25 +1,25 @@
-import { STATS } from "@/constants";
-import styles from "@/style";
+// import { STATS } from "@/constants";
+// import styles from "@/style";
 
-const Stats = () => {
-  return (
-    <section className={`${styles.margins}`}>
-      <div className="flex w-max animate-scrollX whitespace-nowrap gap-10">
-        {[...STATS, ...STATS].map((stat, index) => (
-          <div
-            key={`${stat.id}-${index}`}
-            className="flex-shrink-0 flex items-center justify-center"
-          >
-            <p
-              className={`${styles.flexCenter} orange-text-gradient font-poppins font-normal sm:text-[30px] text-[16px] tracking-wider xs:leading-[53px] leading-[43px] uppercase ml-3`}
-            >
-              {stat.title}
-            </p>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
-};
+// const Stats = () => {
+//   return (
+//     <section className={`${styles.margins}`}>
+//       <div className="flex w-max animate-scrollX whitespace-nowrap gap-10">
+//         {[...STATS, ...STATS].map((stat, index) => (
+//           <div
+//             key={`${stat.id}-${index}`}
+//             className="flex-shrink-0 flex items-center justify-center"
+//           >
+//             <p
+//               className={`${styles.flexCenter} orange-text-gradient font-poppins font-normal sm:text-[30px] text-[16px] tracking-wider xs:leading-[53px] leading-[43px] uppercase ml-3`}
+//             >
+//               {stat.title}
+//             </p>
+//           </div>
+//         ))}
+//       </div>
+//     </section>
+//   );
+// };
 
-export default Stats;
+// export default Stats;
