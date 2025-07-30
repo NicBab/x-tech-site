d0f4a43c6b15576e218bd1ad5342cdce5a9c0317
nickpbab@gmail.com <Nick_Bab>
2025-07-30 15:34:55 -0500

    footer links orange text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => {
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <Link href="/" className="mb-10">
-            <Image src="/X_icon.png" alt="X_icon" width={74} height={29} />
+            <Image src="/X_grey_logo.png" alt="X_icon" width={100} height={100} />
           </Link>
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1 text-white">
             {FOOTER_LINKS.map((columns) => (
@@ -80,7 +80,7 @@ type FooterColumnProps = {
 const FooterColumn = ({ title, children }: FooterColumnProps) => {
   return (
     <div className="flex flex-col gap-5">
-      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      <h4 className="bold-18 whitespace-nowrap orange-text-gradient">{title}</h4>
       {children}
     </div>
   );
