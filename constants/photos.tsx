2779291ac882cd65646da58dbdc143e9319de95b
nickpbab@gmail.com <Nick_Bab>
2025-07-31 08:52:56 -0500

    removed 2 images from hero slider

--- constants/photos.tsx
+++ constants/photos.tsx
@@ -5,11 +5,6 @@ export const PHOTOS = [
     alt: "panel_door",
   },
   {
-    id: "hero-photo-2",
-    src: "/305_panel_1.png",
-    alt: "305_panel",
-  },
-  {
     id: "hero-photo-3",
     src: "/vfd_ctrl_panel.JPG",
     alt: "vfd_ctrl_panel",
@@ -18,11 +13,6 @@ export const PHOTOS = [
     id: "hero-photo-4",
     src: "/vfd_skid.JPG",
     alt: "vfd_skid",
-  },
-  {
-    id: "hero-photo-5",
-    src: "/vfd_glands.JPG",
-    alt: "vfd_glands",
   },
 ];
 
