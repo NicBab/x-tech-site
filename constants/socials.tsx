ae0a7381569fe98289a5bbe60fecf9dd2ff06d26
nickpbab@gmail.com <Nick_Bab>
2025-08-20 08:36:39 -0500

    added facebook and X social icons

--- constants/socials.tsx
+++ constants/socials.tsx
@@ -1,11 +1,13 @@
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import XIcon from "@mui/icons-material/X";
 
 export interface SocialItem {
   title: string;
   href: string;
-  Icon: React.ElementType;          // store the *component type*
-  className: string              // Tailwind color classes live here
+  Icon: React.ElementType; // store the component type
+  className: string; // Tailwind color classes
 }
 
 export const SOCIALS_ICONS = [
@@ -13,12 +15,24 @@ export const SOCIALS_ICONS = [
     title: "linkedin",
     href: "https://www.linkedin.com/company/xtechnology-and-consulting-llc/",
     Icon: LinkedInIcon,
-    color: "rgba(255, 255, 255, 0.7)", // Tailwind blue-500 hex
+    color: "rgba(255, 255, 255, 0.7)",
   },
   {
     title: "instagram",
     href: "https://www.instagram.com/xtechnologyusa/",
     Icon: InstagramIcon,
-    color: "rgba(255, 255, 255, 0.7)", // Tailwind pink-500 hex
+    color: "rgba(255, 255, 255, 0.7)",
+  },
+  {
+    title: "facebook",
+    href: "",
+    Icon: FacebookIcon,
+    color: "rgba(255, 255, 255, 0.7)",
+  },
+  {
+    title: "X",
+    href: "",
+    Icon: XIcon,
+    color: "rgba(255, 255, 255, 0.7)",
   },
 ] as const;
